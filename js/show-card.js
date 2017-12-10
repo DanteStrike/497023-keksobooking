20af1bf9b16a2fb525fa9de9be877bec1aacc2c8
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-11 00:56:17 +0300

    eslint

--- js/show-card.js
+++ js/show-card.js
@@ -1,11 +1,9 @@
 'use strict';
 
 (function () {
-  window.show_card = {
-    showCard: function (target, targets) {
+  window.showCard = function (target, targets) {
       var mapGeneratedCards = document.querySelectorAll('.map__card');
       var offerIndex = [].indexOf.call(targets, target);
       mapGeneratedCards[offerIndex].style.display = '';
-    }
-  };
+    };
 })();
