c7056a7c832c7fe7d88951ded5f1997aee781caa
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-12 18:35:42 +0300

    Определил модуль работы с сервером

--- js/backend.js
+++ js/backend.js
@@ -1,14 +1,43 @@
 'use strict';
 
 (function () {
-  var URL = 'https://1510.dump.academy/keksobooking/data';
+  var SERVER_URL = 'https://1510.dump.academy/keksobooking';
+
+  var setup = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    return xhr;
+  };
 
   var load = function (onLoad, onError) {
+    var xhr = setup(onLoad, onError);
 
+    xhr.open('GET', SERVER_URL + '/data');
+    xhr.send();
   };
 
   var save = function (data, onLoad, onError) {
+    var xhr = setup(onSuccess, onError);
 
+    xhr.open('POST', SERVER_URL);
+    xhr.send(data);
   };
 
   window.backend = {
