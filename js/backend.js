7d046ab3c1d32d8b84c6319b68519c244c575d85
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-12 18:26:39 +0300

    init

---
+++ js/backend.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+  var URL = 'https://1510.dump.academy/keksobooking/data';
+
+  var load = function (onLoad, onError) {
+
+  };
+
+  var save = function (data, onLoad, onError) {
+
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
