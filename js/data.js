c3fc7fee3625ffac9947e4340bd517f402478747
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-14 00:12:50 +0300

    Добавил функцию создающую гибкую форму ошибки

--- js/data.js
+++ js/data.js
@@ -13,6 +13,8 @@
   var MAP_PIN_Y_MIN = 100;
   var MAP_PIN_Y_MAX = 500;
 
+  var LIVE_TIME_ERROR_MESSAGE = 1500;
+
   //  var AVATAR_NUMBERS = [ 1, 2, 3, 4, 5, 6, 7, 8];
   //  Константы для описания предложения
   var OFFER_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
@@ -129,7 +131,48 @@
     });
   };
 
+  //  Форма ошибок схожа для формы и для карты, но имеет отличия в стилизации
+  //  Вынесена в отдельный модуль с гибкой настройкой callback-ом
+  var removeErrorMessage = function () {
+    var currentNodeError = document.querySelector('.ErrorMessage');
+
+    if (currentNodeError) {
+      currentNodeError.parentNode.removeChild(currentNodeError);
+    }
+  };
+
+  var onDefaultError = function (errorMessage, messageLifetime, callback) {
+    var nodeError = document.createElement('div');
+
+    if (!messageLifetime || messageLifetime === 'default') {
+      messageLifetime = LIVE_TIME_ERROR_MESSAGE;
+    }
+
+    nodeError.classList.add('ErrorMessage');
+
+    //  Дефолтные настройки
+    nodeError.style.position = 'fixed';
+    nodeError.style.zIndex = '100';
+    nodeError.style.left = 0;
+    nodeError.style.right = 0;
+    nodeError.style.margin = '0 auto';
+    nodeError.style.textAlign = 'center';
+    nodeError.style.fontSize = '20px';
+    nodeError.style.backgroundColor = 'red';
+    nodeError.textContent = errorMessage;
+
+    //  Гибкая кастомизация сообщения ошибки
+    if (typeof callback === 'function') {
+      callback(nodeError, errorMessage);
+    }
+
+    document.body.insertAdjacentElement('afterbegin', nodeError);
+
+    setTimeout(removeErrorMessage, messageLifetime);
+  };
+
   window.data = {
-    mapPin: generateMapPins(MAP_PIN_COUNT)
+    mapPin: generateMapPins(MAP_PIN_COUNT),
+    onDefaultError: onDefaultError
   };
 })();
