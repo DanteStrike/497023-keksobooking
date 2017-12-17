d40968c14fd41235bd450add72f0389bef856045
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-17 17:51:24 +0300

    Рефакторинг: Вынес все взоимодействие с кнопками в отдельный модуль

--- js/pin.js
+++ js/pin.js
@@ -7,19 +7,62 @@
   var MAP_PIN_X_DELTA = 5;
   var MAP_PIN_Y_DELTA = 40;
 
-  window.pin = {
-    buildMapPinNode: function (mapPin, mapHeight) {
-      var mapPinNode = document.querySelector('template').content.querySelector('.map__pin').cloneNode(true);
-      var mapPinNodeAvatar = mapPinNode.querySelector('img');
+  var buildMapPinNode = function (mapPin, mapHeight) {
+    var mapPinNode = document.querySelector('template').content.querySelector('.map__pin').cloneNode(true);
+    var mapPinNodeAvatar = mapPinNode.querySelector('img');
 
-      mapPinNode.style.left = (mapPin.location.x - MAP_PIN_X_DELTA) + 'px';
+    mapPinNode.style.left = (mapPin.location.x - MAP_PIN_X_DELTA) + 'px';
 
-      // mapHeight высота рабочей области карты, нужна для пересчета координат с верхнего угла в нижний
-      mapPinNode.style.top = (mapHeight - mapPin.location.y - MAP_PIN_Y_DELTA) + 'px';
-      mapPinNodeAvatar.src = mapPin.author.avatar;
+    // mapHeight высота рабочей области карты, нужна для пересчета координат с верхнего угла в нижний
+    mapPinNode.style.top = (mapHeight - mapPin.location.y - MAP_PIN_Y_DELTA) + 'px';
+    mapPinNodeAvatar.src = mapPin.author.avatar;
 
-      return mapPinNode;
+    mapPinNode.style.display = 'none';
+
+    return mapPinNode;
+  }
+
+  var disablePin = function () {
+    var mapPinActive = document.querySelector('.map__pin--active');
+    if (mapPinActive) {
+      mapPinActive.classList.remove('map__pin--active');
+      return true;
+    } else {
+      return false;
     }
+  };
+
+  var onMapPinClick = function (evt) {
+    // Переключить фокус, если необходимо. target - img, его родитель button
+    var target = evt.target.tagName === 'IMG' ? evt.target.parentNode : evt.target;
+    var offerIndex;
+    var mapPinActive = document.querySelector('.map__pin--active');
+    var mapLoadedPins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+
+    if (disablePin()) {
+      window.card.hideCard(mapPinActive, mapLoadedPins);
+    }
+
+    target.classList.add('map__pin--active');
+    window.showCard(target, mapLoadedPins);
+    document.addEventListener('keydown', window.map.onMapEscPress);
+  };
+
+  var onMapPinEnterPress = function (evt) {
+    if (evt.keyCode === window.map.enterKeyCode) {
+      onMapPinClick(evt);
+    }
+  };
+
+  var initializeMapPinEvent = function (mapPinNode) {
+    mapPinNode.addEventListener('click', onMapPinClick);
+    mapPinNode.addEventListener('keydown', onMapPinEnterPress);
+  }
+
+  window.pin = {
+    disablePin: disablePin,
+    buildMapPinNode: buildMapPinNode,
+    initializeMapPinEvent: initializeMapPinEvent,
   };
 })();
 
