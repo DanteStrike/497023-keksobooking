e474f49b7c10f4694634485fe9c21480bb623dcb
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-17 17:52:18 +0300

    Более смысловые имена

--- js/show-card.js
+++ js/show-card.js
@@ -2,8 +2,8 @@
 
 (function () {
   window.showCard = function (target, targets) {
-    var mapGeneratedCards = document.querySelectorAll('.map__card');
+    var mapCards = document.querySelectorAll('.map__card');
     var offerIndex = [].indexOf.call(targets, target);
-    mapGeneratedCards[offerIndex].style.display = '';
+    mapCards[offerIndex].style.display = '';
   };
 })();
