a4e1156e8b48f1339039644e353392924630aefc
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-17 17:50:55 +0300

    Рефакторинг: Вынес все взоимодействие с карточками в отдельный модуль

--- js/card.js
+++ js/card.js
@@ -14,33 +14,70 @@
     }
   };
 
-  window.card = {
-    //  Функция создает DOM элемент шаблона (template) 'article.map__card', согласно массиву объектов mapPins
-    //  mapPin (object) - объект mapPin
-    //  return mapCardNode (object) - вернуть узел
-    buildMapCard: function (mapPin) {
-      var mapCardNode = document.querySelector('template').content.querySelector('article.map__card').cloneNode(true);
-      var mapCardNodeAvatar = mapCardNode.querySelector('.popup__avatar');
-      var mapCardNodeTitle = mapCardNode.querySelector('h3');
-      var mapCardNodeAddress = mapCardNode.querySelector('p:first-of-type');
-      var mapCardNodePrice = mapCardNode.querySelector('.popup__price');
-      var mapCardNodeType = mapCardNode.querySelector('h4');
-      var mapCardNodeRoomsGuests = mapCardNode.querySelector('p:nth-of-type(3)');
-      var mapCardNodeCheckInOut = mapCardNode.querySelector('p:nth-of-type(4)');
-      var mapCardNodeFeatures = mapCardNode.querySelector('.popup__features');
-      var mapCardNodeDescription = mapCardNode.querySelector('p:last-of-type');
+  //  Функция создает DOM элемент шаблона (template) 'article.map__card', согласно массиву объектов mapPins
+  //  mapPin (object) - объект mapPin
+  //  return mapCardNode (object) - вернуть узел
+  var buildMapCard = function (mapPin) {
+    var mapCardNode = document.querySelector('template').content.querySelector('article.map__card').cloneNode(true);
+    var mapCardNodeAvatar = mapCardNode.querySelector('.popup__avatar');
+    var mapCardNodeTitle = mapCardNode.querySelector('h3');
+    var mapCardNodeAddress = mapCardNode.querySelector('p:first-of-type');
+    var mapCardNodePrice = mapCardNode.querySelector('.popup__price');
+    var mapCardNodeType = mapCardNode.querySelector('h4');
+    var mapCardNodeRoomsGuests = mapCardNode.querySelector('p:nth-of-type(3)');
+    var mapCardNodeCheckInOut = mapCardNode.querySelector('p:nth-of-type(4)');
+    var mapCardNodeFeatures = mapCardNode.querySelector('.popup__features');
+    var mapCardNodeDescription = mapCardNode.querySelector('p:last-of-type');
 
-      mapCardNodeAvatar.src = mapPin.author.avatar;
-      mapCardNodeTitle.textContent = mapPin.offer.title;
-      mapCardNodeAddress.textContent = mapPin.offer.address;
-      mapCardNodePrice.textContent = mapPin.offer.price + '	\u20BD/ночь';
-      mapCardNodeType.textContent = mapPin.offer.type;
-      mapCardNodeRoomsGuests.textContent = mapPin.offer.rooms + ' комнаты для ' + mapPin.offer.guests + ' гостей';
-      mapCardNodeCheckInOut.textContent = 'Заезд после ' + mapPin.offer.checkin + ', выезд до ' + mapPin.offer.checkout;
-      buildMapCardFeatures(mapCardNodeFeatures, mapPin.offer.features);
-      mapCardNodeDescription.textContent = mapPin.offer.description;
+    mapCardNodeAvatar.src = mapPin.author.avatar;
+    mapCardNodeTitle.textContent = mapPin.offer.title;
+    mapCardNodeAddress.textContent = mapPin.offer.address;
+    mapCardNodePrice.textContent = mapPin.offer.price + '	\u20BD/ночь';
+    mapCardNodeType.textContent = mapPin.offer.type;
+    mapCardNodeRoomsGuests.textContent = mapPin.offer.rooms + ' комнаты для ' + mapPin.offer.guests + ' гостей';
+    mapCardNodeCheckInOut.textContent = 'Заезд после ' + mapPin.offer.checkin + ', выезд до ' + mapPin.offer.checkout;
+    buildMapCardFeatures(mapCardNodeFeatures, mapPin.offer.features);
+    mapCardNodeDescription.textContent = mapPin.offer.description;
 
-      return mapCardNode;
+    mapCardNode.style.display = 'none';
+
+    return mapCardNode;
+  };
+
+  //  Поиск индекса активной кнопки относительно массива всех сгенерированных кнопок
+  //  Для определения соответствующей этой кнопке предложения
+  var hideCard = function (pinNode, pinNodes) {
+    var mapCards = document.querySelectorAll('.map__card');
+    var offerIndex = [].indexOf.call(pinNodes, pinNode);
+    mapCards[offerIndex].style.display = 'none';
+  };
+
+  var onMapCardClick = function (evt) {
+    var target = evt.target;
+    var pinIndex;
+
+    if (target.classList.contains('popup__close')) {
+      target.parentNode.style.display = 'none';
+      window.pin.disablePin();
+
+      document.removeEventListener('keydown', window.map.onMapEscPress);
     }
+  };
+
+  var onMapCardEnterPress = function (evt) {
+    if (evt.keyCode === window.map.enterKeyCode) {
+      onMapCardClick(evt);
+    }
+  };
+
+  var initializeMapCardEvent = function (mapCardNode) {
+    mapCardNode.addEventListener('click', onMapCardClick);
+    mapCardNode.addEventListener('keydown', onMapCardEnterPress);
+  }
+
+  window.card = {
+    buildMapCard: buildMapCard,
+    hideCard: hideCard,
+    initializeMapCardEvent: initializeMapCardEvent
   };
 })();
