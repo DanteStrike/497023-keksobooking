ae1eaba30e4121a6e0eadf4627b1fba170d2e3d3
Gevork Gevorkyan <bmstu.gg@gmail.com>
2017-12-20 15:14:57 +0300

    Перенес mapPinNodeAvatar для большей логичности

--- js/pin.js
+++ js/pin.js
@@ -15,9 +15,9 @@
 
     // mapHeight высота рабочей области карты, нужна для пересчета координат с верхнего угла в нижний
     mapPinNode.style.top = (mapHeight - mapPin.location.y - MAP_PIN_Y_DELTA) + 'px';
-    mapPinNodeAvatar.src = mapPin.author.avatar;
-
     mapPinNode.style.display = 'none';
+
+    mapPinNodeAvatar.src = mapPin.author.avatar;
 
     return mapPinNode;
   };
